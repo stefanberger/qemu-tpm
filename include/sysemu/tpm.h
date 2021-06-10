a70f7354a97d23796a6747a94f7921e16e796da7
Stefan Berger <stefanb@linux.ibm.com>
2021-06-10 13:48:40 -0400

    sysemu: Make TPM structures inaccessible if CONFIG_TPM is not defined
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- include/sysemu/tpm.h
+++ include/sysemu/tpm.h
@@ -15,9 +15,11 @@
 #include "qapi/qapi-types-tpm.h"
 #include "qom/object.h"
 
-int tpm_config_parse(QemuOptsList *opts_list, const char *optarg);
 int tpm_init(void);
 void tpm_cleanup(void);
+
+#ifdef CONFIG_TPM
+int tpm_config_parse(QemuOptsList *opts_list, const char *optarg);
 
 typedef enum TPMVersion {
     TPM_VERSION_UNSPEC = 0,
@@ -72,5 +74,7 @@ static inline TPMVersion tpm_get_version(TPMIf *ti)
 
     return TPM_IF_GET_CLASS(ti)->get_version(ti);
 }
+
+#endif /* CONFIG_TPM */
 
 #endif /* QEMU_TPM_H */
