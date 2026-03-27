bac715453c9609d6579a7ad09de704914502e811
Stefan Berger <stefanb@linux.ibm.com>
2026-03-27 15:00:52 -0400

    tests: Use ML-DSA-87 operations to cause large TPM transfers with CRB
    To test large data transfers (receiving and sending) that make use of a CRB
    chunked transfer, create an ML-DSA-87 key and sign some data with it and
    receive the 4627 bytes signature. After this send the signature back to the
    TPM to have the TPM verify the signature.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Signed-off-by: Arun Menon <armenon@redhat.com>

--- tests/qtest/tpm-tests.c
+++ tests/qtest/tpm-tests.c
@@ -13,10 +13,13 @@
  */
 
 #include "qemu/osdep.h"
+#include "system/tpm_util.h"
 #include <glib/gstdio.h>
 
 #include "libqtest-single.h"
 #include "tpm-tests.h"
+
+#define ML_DSA_87_SIGNATURE_SIZE 4627
 
 static bool
 tpm_test_swtpm_skip(void)
@@ -139,4 +142,114 @@ void tpm_test_swtpm_migration_test(const char *src_tpm_path,
     tpm_util_swtpm_kill(src_tpm_pid);
     g_unlink(src_tpm_addr->u.q_unix.path);
     qapi_free_SocketAddress(src_tpm_addr);
+}
+
+void tpm_test_swtpm_large_tx_test(const char *src_tpm_path, tx_func *tx,
+                                  const char *ifmodel,
+                                  const char *machine_options)
+{
+    unsigned char signature[2 + 2 + ML_DSA_87_SIGNATURE_SIZE];/*TPMT_SIGNATURE*/
+    unsigned char response[8192];
+    unsigned char request[8192];
+    SocketAddress *addr = NULL;
+    GError *error = NULL;
+    char *args = NULL;
+    GPid swtpm_pid;
+    QTestState *s;
+    gboolean succ;
+
+    if (tpm_test_swtpm_skip()) {
+        return;
+    }
+
+    /* Large transfers based on ML-DSA operations required default-v2 profile */
+    if (!tpm_util_swtpm_has_profile("default-v2", "ml-dsa")) {
+        return;
+    }
+
+    succ = tpm_util_swtpm_start(src_tpm_path, &swtpm_pid, &addr, "default-v2",
+                                &error);
+    g_assert_true(succ);
+
+    args = g_strdup_printf(
+        "%s "
+        "-chardev socket,id=chr,path=%s "
+        "-tpmdev emulator,id=tpm0,chardev=chr "
+        "-device %s,tpmdev=tpm0",
+        machine_options ? : "", addr->u.q_unix.path, ifmodel);
+
+    s = qtest_start(args);
+    g_free(args);
+
+    tpm_util_startup(s, tx);
+
+    static const unsigned char tpm_createprimary_mldsa[] = {
+        0x80, 0x02, 0x00, 0x00, 0x00, 0x38, 0x00, 0x00,
+        0x01, 0x31, 0x40, 0x00, 0x00, 0x07, 0x00, 0x00,
+        0x00, 0x09, 0x40, 0x00, 0x00, 0x09, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x0f, 0x00, 0xa1, 0x00, 0x0b, 0x00,
+        0x04, 0x04, 0x72, 0x00, 0x00, 0x00, 0x03, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+    tx(s, tpm_createprimary_mldsa, sizeof(tpm_createprimary_mldsa),
+       response, sizeof(response));
+    g_assert_cmpint(tpm_cmd_get_errcode(response), ==, 0);
+    g_assert_cmpint(tpm_cmd_get_size(response), ==, 2831);
+
+    static const unsigned char tpm_signsequencestart[] = {
+        0x80, 0x01, 0x00, 0x00, 0x00, 0x12, 0x00, 0x00,
+        0x01, 0xaa, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00
+    };
+    tx(s, tpm_signsequencestart, sizeof(tpm_signsequencestart),
+       response, sizeof(response));
+    g_assert_cmpint(tpm_cmd_get_errcode(response), ==, 0);
+    g_assert_cmpint(tpm_cmd_get_size(response), ==, 14);
+
+    /* Complete sequence and get signature */
+    static const unsigned char tpm_signsequencecomplete[] = {
+        0x80, 0x02, 0x00, 0x00, 0x00, 0x2a, 0x00, 0x00,
+        0x01, 0xa4, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x40, 0x00,
+        0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+        0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00
+    };
+    tx(s, tpm_signsequencecomplete, sizeof(tpm_signsequencecomplete),
+       response, sizeof(response));
+    g_assert_cmpint(tpm_cmd_get_errcode(response), ==, 0);
+    g_assert_cmpint(tpm_cmd_get_size(response), ==, 4655);
+
+    /* TPMT_SIGNATURE found at offset 14 */
+    memcpy(signature, &response[14], sizeof(signature));
+
+    static const unsigned char tpm_verifysequencestart[] = {
+        0x80, 0x01, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00,
+        0x01, 0xa9, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    };
+    tx(s, tpm_verifysequencestart, sizeof(tpm_verifysequencestart),
+       response, sizeof(response));
+    g_assert_cmpint(tpm_cmd_get_errcode(response), ==, 0);
+    g_assert_cmpint(tpm_cmd_get_size(response), ==, 14);
+
+    /* TPM2_VerifySequenceComplete */
+    memcpy(request,
+           (unsigned char[]) {
+               0x80, 0x02, 0x00, 0x00, 0x12, 0x36, 0x00, 0x00,
+               0x01, 0xa3, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00,
+               0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x40, 0x00,
+               0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00,
+           }, 31);
+    memcpy(&request[31], signature, sizeof(signature));
+    tx(s, request, 31 + sizeof(signature), response, sizeof(response));
+    g_assert_cmpint(tpm_cmd_get_errcode(response), ==, 0);
+    g_assert_cmpint(tpm_cmd_get_size(response), ==, 27);
+
+    qtest_end();
+    tpm_util_swtpm_kill(swtpm_pid);
+
+    g_unlink(addr->u.q_unix.path);
+    qapi_free_SocketAddress(addr);
 }
